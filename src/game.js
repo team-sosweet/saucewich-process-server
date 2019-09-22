b55dfef48a366fa15002e9e08b1f0508284a0f3e
huewilliams <4879sinwoo@naver.com>
2019-09-22 14:28:32 +0900

    ADD: pid-port 추가 API

--- src/game.js
+++ src/game.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const request = require('request');
+require('dotenv').config();
 const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 
 const router = express.Router();
 
+const processList = {};
+const portList = {};
 // exec version
 // const process = function() {
 //     console.log('run game server process');
@@ -15,10 +20,24 @@ const router = express.Router();
 //     })
 // };
 
+requestCrash = function(port) {
+    return new Promise((resolve, reject)=>{
+        request.get(`${process.env.MAIN_SERVER}${port}`, (err, response, body)=>{
+            if(err) {
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        })
+    })
+};
+
 // spawn version
-const process = function() {
+const processing = async function() {
     console.log('processing game server');
-    const client = spawn('C:/WindowsServer/SaucewichServer.exe -log');
+    const client = await spawn('C:/WindowsServer/SaucewichServer.exe -log');
+    processList[client.pid] = client;
+    console.log(client.pid);
 
     client.stdout.on('data', (data)=>{
         console.log('stdout : '+data);
@@ -28,14 +47,26 @@ const process = function() {
         console.log('stderr : '+data);
     });
 
-    client.on('exit', (code)=>{
-        console.log('exit: '+ code);
+    client.on('exit', async (code)=>{
+        console.log('exit: '+ client.pid);
+        delete processList[client.pid];
+        console.log('port: '+ portList[client.pid]);
+        await requestCrash(portList[client.pid]);
+        delete portList[client.pid];
     });
 };
 
 router.get('/process', async (req, res, next)=>{
-  await process();
+  await processing();
   res.json({success: true});
+});
+
+router.post('/port/:pid', async (req, res, next)=>{
+    const pid = req.params.pid;
+    const port = req.body.port;
+    portList[pid] = port;
+    res.json({pid: pid, port: portList[pid]});
+    //test[pid].kill();
 });
 
 module.exports = router;
