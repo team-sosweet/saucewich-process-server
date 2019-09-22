2ae6107784454378c4793c1d730dfae4fb4b3dd1
huewilliams <4879sinwoo@naver.com>
2019-09-22 13:04:10 +0900

    ADD: process game spawn version

--- src/game.js
+++ src/game.js
@@ -3,15 +3,34 @@ const exec = require('child_process').exec;
 
 const router = express.Router();
 
+// exec version
+// const process = function() {
+//     console.log('run game server process');
+//     exec('C:/WindowsServer/SaucewichServer.exe -log', (err,data)=>{
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log(data.toString());
+//         }
+//     })
+// };
+
+// spawn version
 const process = function() {
-    console.log('run game server process');
-    exec('C:/WindowsServer/SaucewichServer.exe -log', (err,data)=>{
-        if(err) {
-            console.log(err);
-        } else {
-            console.log(data.toString());
-        }
-    })
+    console.log('processing game server');
+    const client = spawn('C:/WindowsServer/SaucewichServer.exe -log');
+
+    client.stdout.on('data', (data)=>{
+        console.log('stdout : '+data);
+    });
+
+    client.stderr.on('data', (data)=>{
+        console.log('stderr : '+data);
+    });
+
+    client.on('exit', (code)=>{
+        console.log('exit: '+ code);
+    });
 };
 
 router.get('/process', async (req, res, next)=>{
