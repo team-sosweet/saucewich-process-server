7269c84d50948526917d7cb96d82465877e9fc42
huewilliams <4879sinwoo@naver.com>
2019-09-22 16:27:58 +0900

    FIX: processId-port 저장 방식 변경(내부 배열 -> redis)

--- src/game.js
+++ src/game.js
@@ -3,22 +3,12 @@ const request = require('request');
 require('dotenv').config();
 const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
+const redisClient = require('./redisSetting');
+const {getValue, deleteKey} = require('./redisUtil');
 
 const router = express.Router();
 
-const processList = {};
 const portList = {};
-// exec version
-// const process = function() {
-//     console.log('run game server process');
-//     exec('C:/WindowsServer/SaucewichServer.exe -log', (err,data)=>{
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log(data.toString());
-//         }
-//     })
-// };
 
 requestCrash = function(port) {
     return new Promise((resolve, reject)=>{
@@ -36,7 +26,7 @@ requestCrash = function(port) {
 const processing = async function() {
     console.log('processing game server');
     const client = await spawn('C:/WindowsServer/SaucewichServer.exe');
-    processList[client.pid] = client;
+    //const client = await spawn('C:/windows/system32/notepad.exe');
     console.log(client.pid);
 
     client.stdout.on('data', (data)=>{
@@ -49,10 +39,10 @@ const processing = async function() {
 
     client.on('exit', async (code)=>{
         console.log('exit: '+ client.pid);
-        delete processList[client.pid];
-        console.log('port: '+ portList[client.pid]);
-        await requestCrash(portList[client.pid]);
-        delete portList[client.pid];
+        const port = await getValue('process', client.pid);
+        console.log('port: '+ port);
+        await requestCrash(port);
+        console.log(await deleteKey('process', client.pid));
     });
 };
 
@@ -64,9 +54,9 @@ router.get('/process', async (req, res, next)=>{
 router.post('/port/:pid', async (req, res, next)=>{
     const pid = req.params.pid;
     const port = req.body.port;
-    portList[pid] = port;
-    res.json({pid: pid, port: portList[pid]});
-    //test[pid].kill();
+    await redisClient.hset('process', pid, port);
+    //portList[pid] = port;
+    res.json({pid: pid, port: port});
 });
 
 module.exports = router;
